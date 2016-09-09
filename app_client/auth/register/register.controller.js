689c14b19378f1f160bccffa48d4bb5cf34df32a
Donny Mellstrom <donny@decepl.us>
2016-09-09 12:53:47 -0500

    Display message property of error on failed login or registration

--- app_client/auth/register/register.controller.js
+++ app_client/auth/register/register.controller.js
@@ -19,7 +19,7 @@
       authentication
         .register(vm.credentials)
         .error(function(err){
-          alert(err);
+          alert(err.message);
         })
         .then(function(){
           $location.path('profile');
